0dfda49acdcde6225c077d4987d4ba093ad032a8
Bich Le <bcle00@gmail.com>
2019-03-20 05:25:00 +0000

    iptables: optionally exclude internal service and pod IPs

--- containers/mw-proxy-init/configure-iptables.sh
+++ containers/mw-proxy-init/configure-iptables.sh
@@ -2,6 +2,17 @@
 
 set -ex
 
-iptables -t nat -A OUTPUT ! -d 127.0.0.1 -p tcp -j REDIRECT --to-port ${PROXY_PORT} -m owner --uid-owner ${APISERVER_UID}
+if [[ -n "${INTERNAL_SERVICES_CIDR}" ]] ; then
+	FILTER_INTERNAL_SERVICES="! -d ${INTERNAL_SERVICES_CIDR}"
+fi
+
+if [[ -n "${INTERNAL_PODS_CIDR}" ]] ; then
+	FILTER_INTERNAL_PODS="! -d ${INTERNAL_PODS_CIDR}"
+fi
+
+iptables -t nat -A OUTPUT ! -d 127.0.0.1 \
+	${FILTER_INTERNAL_SERVICES} ${FILTER_INTERNAL_PODS} \
+	-p tcp -j REDIRECT --to-port ${PROXY_PORT} \
+	-m owner --uid-owner ${APISERVER_UID}
 
 
