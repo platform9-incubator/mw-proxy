41032db32bc86acdfac226570f66e4e4238788e5
Bich Le <leb@platform9.net>
2019-06-20 12:51:03 -0700

    fix iptables commands in mw-proxy-init
    Use a separate iptables command for the INTERNAL_SERVICES_CIDR
    if declared.

--- containers/mw-proxy-init/configure-iptables.sh
+++ containers/mw-proxy-init/configure-iptables.sh
@@ -3,15 +3,10 @@
 set -ex
 
 if [[ -n "${INTERNAL_SERVICES_CIDR}" ]] ; then
-	FILTER_INTERNAL_SERVICES="! -d ${INTERNAL_SERVICES_CIDR}"
-fi
-
-if [[ -n "${INTERNAL_PODS_CIDR}" ]] ; then
-	FILTER_INTERNAL_PODS="! -d ${INTERNAL_PODS_CIDR}"
+	iptables -t nat -A OUTPUT -d "${INTERNAL_SERVICES_CIDR}" -j ACCEPT
 fi
 
 iptables -t nat -A OUTPUT ! -d 127.0.0.1 \
-	${FILTER_INTERNAL_SERVICES} ${FILTER_INTERNAL_PODS} \
 	-p tcp -j REDIRECT --to-port ${PROXY_PORT} \
 	-m owner --uid-owner ${APISERVER_UID}
 
